a1b693586d9a9f2cb82362bb4cc6bb0f9f05a545
Andrew McDonald <andrewmcdonald42@gmail.com>
2022-08-18 17:57:06 +1000

    Update index.js

--- index.js
+++ index.js
@@ -208,29 +208,19 @@ function timeSince(date) {
     var seconds = Math.floor((new Date() - date) / 1000);
     
     var interval = seconds / 31536000;   
-    if (interval > 1) {
-        return Math.floor(interval) + " years";
-    }
+    if (interval > 1) { return Math.floor(interval) + " years"; }
   
     interval = seconds / 2592000;
-    if (interval > 1) {
-        return Math.floor(interval) + " months";
-    }
+    if (interval > 1) { return Math.floor(interval) + " months"; }
   
     interval = seconds / 86400;
-    if (interval > 1) {
-        return Math.floor(interval) + " days";
-    }
+    if (interval > 1) { return Math.floor(interval) + " days"; }
   
     interval = seconds / 3600;
-    if (interval > 1) {
-        return Math.floor(interval) + " hours";
-    }
+    if (interval > 1) { return Math.floor(interval) + " hours"; }
     
     interval = seconds / 60;
-    if (interval > 1) {
-        return Math.floor(interval) + " minutes";
-    }
+    if (interval > 1) { return Math.floor(interval) + " minutes"; }
     
     return Math.floor(seconds) + " seconds";
 }
